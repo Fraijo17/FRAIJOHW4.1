79998d05b547e0e1feaf38486b5a4b866a91390a
Fraijo17 <abrahamfraijo17@gmail.com>
2020-10-14 02:16:59 -0600

    had to recreate new repo due to other one not letting me select master branch

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# FRAIJOHW4.1
+# FRAIJOHW4
+
+U of U HW4
+
+This is a single page application with 5 generic Javascript questions. 
+It is intended to do a quick assesment of Javascript.
+Timer is set at 75 seconds and decrements of 10 seconds with every incorrect answer. 
+If question is answered correctly, the timer does not change. 
+Final score is based upon the total remaining time.
+
+In assets/images you can see the file attached is an image of the HTML running. 
+
+
+
---
+++ questions.js
@@ -0,0 +1,35 @@
+var questions = [
+
+    {
+        title: "Question 1: Between JavaScript, ASP script, and jQuery which is faster?",
+        choices: ["JavaScript", "ASP", "jQuery", "All of them"],
+        answer: "JavaScript"
+    },
+
+    {
+        title: "Question 2: Which company developed JavaScript",
+        choices: ["Netgear", "Steam", "Netscape", "Microsoft"],
+        answer: "Netscape"
+    },
+
+    {
+        title: "Question 3: Every statement in Javascript must end with _____",
+        choices: [";", ".", ">", "<"],
+        answer: ";"
+    },
+
+    {
+        title: "Question 4: Does JavaScript support automatic type conversion T/F?",
+        choices: ["True", "False", "Maybe", "I don't know"],
+        answer: "True"
+    },
+
+    {
+        title: "Strings are an array of characters",
+        choices: ["True", "False", "Maybe", "What is an array?"],
+        answer: "True"
+
+    }
+
+
+];
